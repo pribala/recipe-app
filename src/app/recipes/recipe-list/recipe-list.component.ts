d541eedcd735569319c7d6c48aa72079a229ebe2
pribala <andu_pri@yahoo.com>
2018-09-23 19:20:55 -0400

    recipe item template

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../_interfaces/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  // tslint:disable-next-line:max-line-length
+  recipes: Recipe[] = [new Recipe('Burrito', 'Healthy Veggie Burrito', 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+                       // tslint:disable-next-line:max-line-length
+                       new Recipe('Dosa', 'Crunchy Rice & Lentil Crepe', 'https://images.pexels.com/photos/221143/pexels-photo-221143.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+                       // tslint:disable-next-line:max-line-length
+                       new Recipe('Curry', 'Potato & Peas Curry', 'https://images.pexels.com/photos/674574/pexels-photo-674574.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')];
   constructor() { }
 
   ngOnInit() {
